1f8faae2df15b713fef8a1190e7d9323569fd6d0
guodaxia9527 <liyongqiang9527@gmail.com>
2026-05-09 13:05:39 +0800

    feat: 回声 PWA 发音练习应用

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'echo-v1.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if (resp && resp.status === 200) {
+        const clone = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+      }
+      return resp;
+    })).catch(() => {
+      if (e.request.mode === 'navigate') {
+        return caches.match('./index.html');
+      }
+    })
+  );
+});
